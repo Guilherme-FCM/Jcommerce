250c0493bc635d54fc8b7c86d41183dca69328b8
Guilherme-FCM <guifcm12@gmail.com>
2022-08-05 11:16:37 -0300

    Create connection class

---
+++ src/main/java/br/edu/ifto/model/dao/MyConnection.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifto.Products.model.dao;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author aluno
+ */
+public class MyConnection {
+    public static Connection connect(){
+        JDBC connection = new MysqlConnection();
+        return connection.createConnection();
+    }
+}
