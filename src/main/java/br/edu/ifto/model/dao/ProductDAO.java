38a0a621e04e919be2b228f22481ab608fa71e73
Guilherme-FCM <guifcm12@gmail.com>
2022-08-05 11:16:54 -0300

    Create product dao

---
+++ src/main/java/br/edu/ifto/model/dao/ProductDAO.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifto.Products.model.dao;
+
+import br.edu.ifto.Products.model.entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aluno
+ */
+public class ProductDAO {
+    Connection connection;
+    
+    public ProductDAO(){
+        connection = MyConnection.connect();
+    }
+
+    public List<Product> listProducts() {
+        try {
+            //comando sql
+            String sql = "select * from products";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            //ResultSet, representa o resultado do comando SQL
+            ResultSet rs = ps.executeQuery();
+            //cria uma lista de pessoas para retornar
+            List<Product> pessoas = new ArrayList();
+            //laço para buscar todas as pessoas do banco
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getLong("id"));
+                p.setName(rs.getString("name"));
+                //add pessoa na lista
+                pessoas.add(p);
+            }
+            //retorna a lista de pessoas
+            return pessoas;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
