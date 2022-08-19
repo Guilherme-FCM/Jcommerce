7f162ce974e0035874ca691c39e7cfc037222177
Guilherme-FCM <guifcm12@gmail.com>
2022-08-18 22:01:41 -0300

    Move home template to templates package

---
+++ src/main/java/br/edu/ifto/ConfigurationSpringMvc.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifto;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Configuration para indicar ao Spring que essa é uma classe de configuração.
+ * Em seguida, é preciso estender a classe WebMvcConfigurerAdapter.
+ * @author fagno
+ */
+@Configuration
+public class ConfigurationSpringMvc implements WebMvcConfigurer{
+
+    
+    /**
+     * Com a chamada a registry.addViewController(), estamos registrando um controller automático,
+     * definido pelo próprio Spring, para atender a requisições direcionadas à URL / e /home. E com a chamada 
+     * a setViewName(), sempre que a aplicação receber uma requisição para um desses endereços, a view home, 
+     * criada na última aula, será exibida.
+     * @param registry 
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+
+    }
+
+}
