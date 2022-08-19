866e4807ea10979210015b14b043a50c5d196249
Guilherme-FCM <guifcm12@gmail.com>
2022-08-18 21:46:34 -0300

    Modify product entity to use JPA

--- src/main/java/br/edu/ifto/model/entity/Product.java
+++ src/main/java/br/edu/ifto/model/entity/Product.java
@@ -4,11 +4,23 @@
  */
 package br.edu.ifto.model.entity;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author aluno
  */
-public class Product {
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private double price;
