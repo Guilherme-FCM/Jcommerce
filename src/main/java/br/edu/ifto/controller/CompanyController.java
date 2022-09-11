e5b093535ef74a2392fe645b4cb8d3fb237520c6
Guilherme-FCM <guifcm12@gmail.com>
2022-09-11 17:26:43 -0300

    Add Company CRUD

--- src/main/java/br/edu/ifto/controller/CompanyController.java
+++ src/main/java/br/edu/ifto/controller/CompanyController.java
@@ -4,10 +4,61 @@
  */
 package br.edu.ifto.controller;
 
+import br.edu.ifto.model.entity.Company;
+import br.edu.ifto.model.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  *
  * @author GuilhermeFCM
  */
+@Controller
+@Transactional
+@RequestMapping("companies")
 public class CompanyController {
-    
+    @Autowired
+    CompanyRepository repository;
+
+    @GetMapping
+    public ModelAndView findAll(ModelMap model){
+        model.addAttribute("companies", repository.findAll());
+        return new ModelAndView("/companies/list", model);
+    }
+
+    @GetMapping("/form")
+    public String form(Company company){
+        return "/companies/form";
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(Company company){
+        repository.create(company);
+        return new ModelAndView("redirect:/companies");
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable Long id, ModelMap model) {
+        model.addAttribute("company", repository.find(id));
+        return new ModelAndView("/companies/form", model);
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(Company company){
+        repository.update(company);
+        return new ModelAndView("redirect:/companies");
+    }
+
+    @GetMapping("/remove/{id}")
+    public ModelAndView remove(@PathVariable Long id){
+        repository.remove(id);
+        return new ModelAndView("redirect:/companies");
+    }
 }
--- src/main/java/br/edu/ifto/model/repository/CompanyRepository.java
+++ src/main/java/br/edu/ifto/model/repository/CompanyRepository.java
@@ -4,10 +4,32 @@
  */
 package br.edu.ifto.model.repository;
 
+import br.edu.ifto.model.entity.Company;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author GuilhermeFCM
  */
+@Repository
 public class CompanyRepository {
-    
+    @PersistenceContext
+    EntityManager em;
+
+    public List<Company> findAll(){
+        Query query = em.createQuery("from Company");
+        return query.getResultList();
+    }
+
+    public Company find(Long id){ return em.find(Company.class, id); }
+
+    public void create(Company company){ em.persist(company); }
+
+    public Company update(Company company){ return em.merge(company); }
+
+    public void remove(Long id) { em.remove( this.find(id) ); }
 }
