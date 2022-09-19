677abb7033b693985f81a73f477a3bc8bae58e75
Guilherme-FCM <guifcm12@gmail.com>
2022-09-18 23:05:24 -0300

    New home view

--- src/main/java/br/edu/ifto/ConfigurationSpringMvc.java
+++ src/main/java/br/edu/ifto/ConfigurationSpringMvc.java
@@ -27,8 +27,8 @@ public class ConfigurationSpringMvc implements WebMvcConfigurer{
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/home").setViewName("home");
 
     }
 
--- src/main/java/br/edu/ifto/ProductsApplication.java
+++ src/main/java/br/edu/ifto/ProductsApplication.java
@@ -1,23 +1,27 @@
 package br.edu.ifto;
 
+import br.edu.ifto.model.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @SpringBootApplication
 public class ProductsApplication {
+    @Autowired
+    ProductRepository repository;
     public static void main(String[] args) {
         SpringApplication.run(ProductsApplication.class, args);
     }
         
-    @GetMapping("/hello")
-    public String hello(
-        @RequestParam(value = "name", defaultValue = "World") 
-        String name
-    ){
-        return String.format("Hello %s.", name);
+    @GetMapping
+    public ModelAndView index(ModelMap model){
+        model.addAttribute("products", repository.findProducts());
+        return new ModelAndView("/home", model);
     }
 }
