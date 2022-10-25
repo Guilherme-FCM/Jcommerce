107be4733a3dc1f9f5ca789325ee0c2d5ab240b7
Guilherme-FCM <guifcm12@gmail.com>
2022-10-24 23:37:05 -0300

    Remove Date import

--- src/main/java/br/edu/ifto/model/entity/Sale.java
+++ src/main/java/br/edu/ifto/model/entity/Sale.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
 
@@ -28,7 +27,7 @@ public class Sale implements Serializable {
     private Long id;
     
     private LocalDate date = LocalDate.now();
-    
+
     @OneToMany(mappedBy = "sale", cascade = CascadeType.PERSIST)
     private List<Item> items = new ArrayList();
 
