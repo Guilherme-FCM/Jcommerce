1d7cf096bdf2fc4c9219a3887be0059e2febb783
Guilherme-FCM <guifcm12@gmail.com>
2022-11-10 21:18:27 -0300

    Crate method findByDescription

--- src/main/java/br/edu/ifto/model/repository/ProductRepository.java
+++ src/main/java/br/edu/ifto/model/repository/ProductRepository.java
@@ -6,9 +6,12 @@ package br.edu.ifto.model.repository;
 
 import br.edu.ifto.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 /**
  *
  * @author GuilhermeFCM
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByDescriptionContaining(String description);
 }
